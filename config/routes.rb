953581b23758fe2f269a6eea8d56d0e20bb36743
pedromtamaral <pedroamaral274@gmail.com>
2014-07-20 22:47:34 +0100

    Task C1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 RailsDepot::Application.routes.draw do
+  get "store/index"
+
   resources :products
 
-
+  root :to => "store#index", as: "store"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
