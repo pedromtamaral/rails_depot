cfc73519cd5c02d85af858c72a965a3603830653
pedromtamaral <pedroamaral274@gmail.com>
2014-07-23 20:29:48 +0100

    Task E1

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,14 @@
 class Cart < ActiveRecord::Base
   # attr_accessible :title, :body
   has_many :line_items, dependent: :destroy
+  
+  def add_product(product_id)
+    item = line_items.find_by_product_id(product_id)
+    if item
+      item.quantity += 1
+    else
+      item = line_items.build(product_id: product_id)
+    end
+    item
+  end
 end
